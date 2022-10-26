fba09425edca885796ec0484447c16d22e1fb571
David Barnett <david@barnett.net.nz>
2022-10-26 20:16:36 +1100

    update cmp_nvim_lsp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,8 +1,4 @@
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
 
 -- Telescope
 require('telescope').setup {
@@ -54,12 +50,11 @@ local on_attach = function(_, bufnr)
     vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
 end
 
--- nvim-cmp supports additional completion capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Enable the following language servers
-local servers = { 'rust_analyzer', 'pyright', 'gopls', 'tsserver', 'tflint' }
+local servers = { 'rust_analyzer', 'pyright', 'gopls', 'tsserver', 'tflint', 'jsonnet_ls' }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
         on_attach = on_attach,
